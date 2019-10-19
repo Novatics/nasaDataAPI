d7e2dd8461d23a3219b2b7710b6157d34a3e8c83
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-10-19 02:19:17 -0300

    fixed include import and serializer name

--- nasaData/core/serializers.py
+++ nasaData/core/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Satellite
 
-class StudentSerializer(serializers.ModelSerializer):
+class SatelliteSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Satellite
