b1f8718ef042c78346d3f81103d1f6e0ef6c0d8c
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-10-19 02:50:54 -0300

    fixed runserver app name

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py makemigrations
-python manage.py makemigrations api
+python manage.py makemigrations core
 python manage.py migrate
-python manage.py migrate api
+python manage.py migrate core
 python3 manage.py runserver 0.0.0.0:${PORT}
