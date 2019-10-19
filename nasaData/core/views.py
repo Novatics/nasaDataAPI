66bcc0cf33f68259a666e9f597e4cb4ac20a5a18
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-10-19 02:55:58 -0300

    updated satellites endpoints renderer

--- nasaData/core/views.py
+++ nasaData/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework import renderers
 from .models import Satellite
 from .serializers import SatelliteSerializer
 
@@ -8,5 +9,8 @@ class SatelliteViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `detail` actions.
     """
+
+    renderer_classes = [renderers.JSONRenderer]
+
     queryset = Satellite.objects.all()
     serializer_class = SatelliteSerializer
