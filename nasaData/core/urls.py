ae60b0454b1e8b89b3da37d5967930d0370c3790
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-10-19 15:37:56 -0300

    added coordinates endpoint

--- nasaData/core/urls.py
+++ nasaData/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import SatelliteViewSet
+from .views import SatelliteViewSet, CoondinatesViewSet
 from django.conf.urls import include
 
 router = DefaultRouter()
 router.register(r'satellites', SatelliteViewSet, basename='sattelite')
+router.register(r'coordinates', CoondinatesViewSet, basename='coordinate')
+
 urlpatterns = router.urls
--- nasaData/core/views.py
+++ nasaData/core/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework import renderers
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 from .models import Satellite
 from .serializers import SatelliteSerializer
 
-from .data_manager import load_data
+from .data_manager import load_data, get_satellite_coordinates
+
 
 class SatelliteViewSet(viewsets.ViewSet):
 
@@ -28,3 +33,38 @@ class SatelliteViewSet(viewsets.ViewSet):
         satellite = get_object_or_404(Satellite, pk=pk)
         serializer = SatelliteSerializer(satellite)
         return Response(serializer.data)
+
+
+class CoondinatesViewSet(viewsets.ViewSet):
+
+    renderer_classes = [renderers.JSONRenderer]
+
+    def retrieve(self, request, pk=None):
+
+        if(self.request.query_params.get('lon') == None
+            or self.request.query_params.get('lat') == None
+            or self.request.query_params.get('alt') == None ):
+            print("NAO TEM TUDO")
+            data = {
+                'id':pk,
+                'coordinates':None,
+            }
+
+        else:
+
+            longitude = float(request.query_params.get('lon'))
+            latitude = float(request.query_params.get('lat'))
+            altitude = float(request.query_params.get('alt'))
+
+            coordinates = get_satellite_coordinates(longitude, latitude, altitude, pk)
+            print(coordinates)
+            print(coordinates[0])
+            data = {
+                'id':pk,
+                'coordinates':{
+                    'azi':coordinates[0],
+                    'alt':coordinates[1],
+                },
+            }
+
+        return Response(data)
