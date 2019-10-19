74843b9db1e798f4e42da674c780842b8fdf90b3
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2019-10-19 01:30:16 -0300

    added first version of satellite model

--- nasaData/core/models.py
+++ nasaData/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Satellite(models.Model):
+
+    id = models.AutoField(
+        primary_key=True
+    )
+
+    satellite_type = models.CharField(
+        ('Satellite Type'),
+		max_length=25,
+        blank=False,
+    )
+
+    name = models.CharField(
+		('Name'),
+		help_text=("Satellite Name"),
+		max_length=50,
+        blank=False,
+	)
+
+    description = models.TextField(
+		('Description'),
+		help_text=("Satellite Description"),
+        blank=False,
+	)
+
+    launch_date = models.DateField(
+        ('Launch Date'),
+        blank=False,
+    )
